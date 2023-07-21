184f5e10478a49ace021e7faea9914352cbeefae
Joel Ric <joemisa16@gmail.com>
2023-07-21 20:55:27 +0800

    test

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 const Header = ({ title }) => {
-	return <div className="bg-slate-300 text-4xl p-5">{title}</div>;
+	return <div className="bg-slate-300 text-4xl p-5">{title}hello</div>;
 };
-
 
 export default Header;
